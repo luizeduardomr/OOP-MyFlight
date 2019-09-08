b4d0ca532ce47690d73d5a38e20ccbb49e19edcb
Lucas Cardoso <lucasgavirachicc@gmail.com>
2019-09-08 13:24:13 -0300

    Adição da App (Página principal) do sistema, para testes

--- App/App.java
+++ App/App.java
@@ -5,18 +5,48 @@ public class App {
 
 	public static void main(String[] args) {
 
+
 		System.out.println("MyFlight project...");
 
+        GerenciadorCias cias = new GerenciadorCias();
+		CiaAerea c1  = new CiaAerea ("G3", "Gol");
+        CiaAerea c2  = new CiaAerea ("JJ", "LATAM");
+        CiaAerea c3  = new CiaAerea ("AZ", "Azul");
 
-		/*Rota rota1 = new Rota();
-		LocalDateTime agora = LocalDateTime.now();
-		Duration duracao = new Duration();
+        cias.inserir(c1);
+        cias.inserir(c2);
+        cias.inserir(c3);
 
-		//public Voo(Rota rota, LocalDateTime dataHora, Duration duracao)
-		Voo v1 = new Voo(3212 );
-		VooVariasEscalas v2 = new VooVariasEscalas(rota1, LocalDateTime, Duration.ofDays(5));
+        for (CiaAerea c: cias.getCias()){
+            System.out.println(c + "\n");
+        }
+
+        Geo portoAlegre = new Geo(23,22);
+        Geo saoPaulo = new Geo (24, 26);
+        Geo london = new Geo (35,39);
+
+        Aeroporto portoAlegreAero = new Aeroporto ("POA", "Porto Alegre", portoAlegre);
+        Aeroporto saoPauloAero = new Aeroporto ("GRU", "São Paulo", saoPaulo);
+        Aeroporto londonAero = new Aeroporto ("LON" , "Londres", london);
+
+        Aeronave aviao1 = new Aeronave ("B747", "Boing 747", 200);
+
+        Rota origem = new Rota (portoAlegreAero, saoPauloAero, aviao1, c1);
+        Rota destino = new Rota (saoPauloAero, londonAero, aviao1, c1);
 
 
-		 */
+        LocalDateTime datahora = LocalDateTime.of(2019, 6, 30, 12, 00);
+        Duration duracao = Duration.ofMinutes(120); // 2 horas
+
+
+/*--------------------------------------------------------------------------*/
+        VooEscalas vooTeste = new VooEscalas(origem, datahora, duracao);
+        vooTeste.adicionarRota(destino);
+        System.out.print (vooTeste);
+
+		VooDireto vooTeste2 = new VooDireto(origem, datahora, duracao);
+		System.out.print (vooTeste2);
+
+		 
 	}
 }
