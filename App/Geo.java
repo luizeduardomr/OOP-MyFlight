6325b114a2f88f2a372bfeba5849ae434a727cfb
LucasGCardoso <lucasgavirachicc@gmail.com>
2019-09-18 13:26:04 -0300

    Exercício 2 de sobrecarga feito. Modificação da classe Geo.

--- App/Geo.java
+++ App/Geo.java
@@ -4,7 +4,7 @@ public class Geo {
     private double latitude;
 
 
-    public  Geo(int longitude, int latitude){
+    public  Geo(double longitude, double latitude){
         this.longitude = longitude;
         this.latitude = latitude;
     }
@@ -19,6 +19,66 @@ public class Geo {
 
     public String toString(){
         return "longitude: " + this.longitude + ", latitude: " + this.latitude;
+    }
+
+
+
+    public static double distancia(double latX, double latY, double longX, double longY) {
+
+        //raio Terra
+        double raioTerra = 6371;
+
+        //converte para radianos as coordenadas dos aeroportos de origem e destino
+        double latitudeRadX = latX;
+        double longitudeRadX = longX;
+        double latitudeRadY = latY;
+        double longitudeRadY = longY;
+
+        //calcula senos e cossenos
+        double senoLatitudes = Math.sin((latitudeRadX-latitudeRadY)*1.0)/2;
+        double senoLongitudes = Math.sin((longitudeRadX-longitudeRadY)*1.0)/2;
+        double cossenoLatOrigem = Math.cos(latitudeRadX);
+        double cossenoLatDestino = Math.cos(latitudeRadY);
+
+        //calcula a parte interna da raiz da fórmula de haversine
+        double raiz = senoLatitudes*senoLatitudes + senoLongitudes*senoLongitudes*cossenoLatOrigem*cossenoLatDestino;
+
+        //calcula a distância
+        double distancia = 2*raioTerra*Math.asin(Math.sqrt(raiz));
+
+        return distancia;
+    }
+
+
+    public static double distancia(Geo origem, Geo destino) {
+
+        //raio Terra
+        double raioTerra = 6371;
+
+        //converte para radianos as coordenadas dos aeroportos de origem e destino
+        double latitudeRadOrigem = Math.toRadians(origem.getLatitude());
+        double longitudeRadOrigem = Math.toRadians(origem.getLongitude());
+        double latitudeRadDestino = Math.toRadians(destino.getLatitude());
+        double longitudeRadDestino = Math.toRadians(destino.getLongitude());
+
+        //calcula senos e cossenos
+        double senoLatitudes = Math.sin((latitudeRadOrigem-latitudeRadDestino)*1.0)/2;
+        double senoLongitudes = Math.sin((longitudeRadOrigem-longitudeRadDestino)*1.0)/2;
+        double cossenoLatOrigem = Math.cos(latitudeRadOrigem);
+        double cossenoLatDestino = Math.cos(latitudeRadDestino);
+
+        //calcula a parte interna da raiz da fórmula de haversine
+        double raiz = senoLatitudes*senoLatitudes + senoLongitudes*senoLongitudes*cossenoLatOrigem*cossenoLatDestino;
+
+        //calcula a distância
+        double distancia = 2*raioTerra*Math.asin(Math.sqrt(raiz));
+
+        return distancia;
+    }
+
+    public double distancia(Geo geoLocal) {
+        Geo dados = new Geo(this.longitude, this.latitude);
+        return Geo.distancia(dados, geoLocal);
     }
 
 }
